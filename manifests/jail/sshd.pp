3fd33d0e81bcfa1a3d0b72c73b61e179b64e691e
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-06-10 13:51:40 +0200

    changed sshd jail settings

--- manifests/jail/sshd.pp
+++ manifests/jail/sshd.pp
@@ -1,11 +1,13 @@
 class fail2ban::jail::sshd(
   $ban_time = 3600,
+  $find_time = 1200,
   $log_path = $::fail2ban::common_paths::syslog_authpriv,
   $conf = {}
 ) inherits fail2ban::common_paths {
   fail2ban::jail{'sshd':
     port    => 'ssh',
     ban_time => $ban_time,
+    find_time => $find_time,
     log_path => $log_path,
     conf    => $conf
   }
